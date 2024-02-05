2acf1452bb33166df85089e5d93dae62fc2692c4
William Tucker <william.tucker29@gmail.com>
2024-02-05 12:52:05 -0600

    Worked through exercise and added all functionality. List items bold when hovered over and return to normal state when not hovered over.

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // TODO
+// const App = () => <div>Some cliche salutation</div>;
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementbyId(
+//     'existing-dom-element-into-which-I-want-to-render-my-component'
+//   )
+// );
+
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+
+  const style = {
+    'font-weight': isDone ? 'bold' : 'normal',
+  };
+
+  return (
+    // No onHover handler. Use onMouseEnter and onMouseLeave
+    <li style={style} onMouseEnter={() => setIsDone(!isDone)} onMouseLeave={() => setIsDone(!isDone)}>
+      {props.groceryItem}
+    </li>
+  );
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map((groceryItem) => (
+      <GroceryListItem groceryItem={groceryItem} />
+      ))}
+  </ul>
+);
+
+
+
+const Grocery = () => (
+    <GroceryList groceryItems={['Chips', 'Spicy Salsa', 'Queso', 'Napkins']} />
+);
+
+ReactDOM.render(
+  <Grocery />,
+  document.getElementById(
+    'app'
+  )
+);
